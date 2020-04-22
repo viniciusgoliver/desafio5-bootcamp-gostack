c3b0db7f2b5f0c42b33f9406bf2654ba511649d1
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2020-04-22 17:29:31 -0300

    Subindo Desafio

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionsRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+
+transactionsRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionsRouter.post('/', (request, response) => {
+  try {
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({ title, value, type });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionsRouter;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,39 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const typesVal = ['income', 'outcome'];
+    if (!typesVal.includes(type)) {
+      throw Error('Type de Trasaction invalido');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Saldo Insuficiente');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
